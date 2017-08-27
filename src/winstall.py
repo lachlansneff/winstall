ac88b6d1cdb49a383aca9fabfcb218354d08cea0
Lachlan Sneff <lachlan.sneff@gmail.com>
2017-08-27 08:55:50 -0400

    Should work now

--- src/winstall.py
+++ src/winstall.py
@@ -1,26 +1,26 @@
 import tempfile, requests, subprocess
 
-class Download:
-    def __init__(self, url):
-        self.url = url
-        r = requests.get(self.url, stream=True)
-        with tempfile.TemporaryFile() as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-            self.tempfile = f
-    def execute(self):
-        self.process = subprocess.Popen([self.tempfile.name])
-    def wait(self):
-        self.process.wait()
+def download(url):
+    r = requests.get(self.url, stream=True)
+    with tempfile.TemporaryFile() as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+        return f
 
-class Winstall:
-    def __init__(self, url, isInstaller=True):
-        self.url = url
-        self.isInstaller = isInstaller
-    def install(self):
-        if (isInstaller):
-            dl = Download(self.url)
-            dl.execute()
-            dl.wait()
-            return True
+def install(tempfile):
+    process = subprocess.Popen([tempfile.name], creationflag=CREATE_NO_WINDOW)
+    return process
+
+urls = {
+    'hyper': 'https://releases.hyper.is/download/win',
+    'git': 'https://central.github.com/deployments/desktop/desktop/latest/win32',
+    'atom': 'https://atom.io/download/windows_x64'
+}
+
+for name, url in urls.items():
+    print('Downloading '+name+'...')
+    f = download(url)
+    print('Installing...')
+    install(f).wait()
+    print(name+' installed')
